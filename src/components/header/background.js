f093e479fb87679a6f6b4ae84015b9b717397804
Don <djo446@mail.umkc.edu>
2020-09-09 09:13:05 -0500

    Added personal information, modified assets.

--- src/components/header/background.js
+++ src/components/header/background.js
@@ -7,8 +7,8 @@ const StyledBackground = styled.div`
   background-size:cover;
   background-position: center;
   background-repeat: no-repeat;
-  background-image: url(${props => props.imageUrl});
-  filter: grayscale(1) blur(2px);
+  background-image: url(https://images.squarespace-cdn.com/content/v1/571fc5edd210b89083925aba/1559599181675-4MQHUP31SG13I9W04NH7/ke17ZwdGBToddI8pDm48kHu-gQqVtNHyDZW_Bn_Cecp7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0kMlYkjvFlctRdmAM11rxFSYXH9LI4fTdHB98XNnOeA6MMLMLVbi4gExhPrIx0rDpQ/LiamWong_EveningsInTokyo.jpg?format=2500w);
+  filter: grayscale(1);
 `
 
 export const Background = props => <StyledBackground {...props} role="img" aria-label="Header background" />
